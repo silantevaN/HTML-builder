46e793720b3dedc9b3b40dd4cbc1281dfb7586f5
anastasiasilanteva <anastasiasilantevaa@gmail.com>
2021-11-03 21:35:36 +0300

    init: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, './secret-folder');
+
+fs.readdir((filePath), (err, files) => {
+  if (err) throw err;
+  for (let file of files) {
+      let fileName = file.split('.')[0];
+      let fileExtname = path.extname(file).split('.')[1];
+      var pathFile = filePath + "/" + file;
+      fs.stat(pathFile, (error, stats) => {
+        if (error) {
+          console.log(error);
+        }
+        else if (stats.isFile()) {
+          console.log(fileName + ' - ' +  fileExtname + ' - ' + stats.size + 'B');
+        }
+      });
+  };
+});
