2b57077998b340ae34cbfe96175627bb0aa68b0f
anastasiasilanteva <anastasiasilantevaa@gmail.com>
2021-11-04 21:10:38 +0300

    init: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const pathCurrentFolder = path.join(__dirname, 'files');
+const pathCopiedFolder = path.join(__dirname, 'files-copy');
+
+
+function listObjects(pathFolder){
+
+  fs.readdir(pathFolder, (err, files) => {
+
+     if(err) throw err;
+     for (let file of files) {
+
+      fs.unlink(pathCopiedFolder + '/' + file, function(err) {
+        if (err) throw err;
+        listObjects(pathCopiedFolder);
+        fs.readdir((pathCurrentFolder), (err, files) => {
+          if (err) throw err;
+          files.forEach(file => {
+            fs.copyFile(pathCurrentFolder + '/' + file, pathCopiedFolder + '/' + file, function(err) {
+              if (err) throw err;
+            });
+          });
+        });
+      });
+    };
+  });
+}
+
+fs.access(pathCopiedFolder, (err) => {
+ if (err) {
+  fs.mkdir(pathCopiedFolder, err => {
+    if (err) throw err;
+    console.log('Folder copied');
+    fs.readdir((pathCurrentFolder), (err, files) => {
+      if (err) throw err;
+      files.forEach(file => {
+        fs.copyFile(pathCurrentFolder + '/' + file, pathCopiedFolder + '/' + file, function(err) {
+          if (err) throw err;
+        });
+      });
+    });
+ });
+ } 
+ listObjects(pathCopiedFolder);
+});
+
+
 
